c1e86fa568378e67d1336e19c0ee6f0490a34c3f
Maxime Fabre <ehtnam6@gmail.com>
2013-07-16 18:12:14 +0200

    Update path to Bower components

--- build.js
+++ build.js
@@ -1,11 +1,11 @@
 ({
   baseUrl: "assets/js",
   paths: {
-		'jquery'          : "../../components/jquery/jquery.min",
-		'marked'          : "../../components/marked/lib/marked",
-		'rainbow'         : "../../components/rainbow/js/rainbow.min",
-		'rainbow-generic' : "../../components/rainbow/js/language/generic",
-		'rainbow-php'     : "../../components/rainbow/js/language/php",
+		'jquery'          : "../../bower_components/jquery/jquery.min",
+		'marked'          : "../../bower_components/marked/lib/marked",
+		'rainbow'         : "../../bower_components/rainbow/js/rainbow.min",
+		'rainbow-generic' : "../../bower_components/rainbow/js/language/generic",
+		'rainbow-php'     : "../../bower_components/rainbow/js/language/php",
   },
   optimize: "uglify2",
   name: "index",
