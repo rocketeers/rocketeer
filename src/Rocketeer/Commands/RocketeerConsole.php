51eba6600c344393dd4ec2b987a0108eae1d475a
Maxime Fabre <ehtnam6@gmail.com>
2013-08-02 18:04:02 +0200

    Allow the passing of runtime configuration

--- src/Rocketeer/Commands/RocketeerConsole.php
+++ src/Rocketeer/Commands/RocketeerConsole.php
@@ -26,11 +26,21 @@ class RocketeerConsole extends Application
 	/**
 	 * Create the Rocketeer CLI
 	 *
+	 * @param array $runtime Runtime configuration
+	 *
 	 * @return Application
 	 */
-	public static function make()
+	public static function make($runtime = array())
 	{
 		$app = RocketeerServiceProvider::make();
+
+		// Set base path
+		$app['path.base'] = explode('/vendor', __DIR__)[0];
+
+		// Merge runtime configuration
+		$config = $app['config']->get('rocketeer::config');
+		$config = array_replace_recursive($config, $runtime);
+		$app['config']->set('rocketeer::config', $config);
 
 		return $app['rocketeer.console'];
 	}
@@ -38,10 +48,12 @@ class RocketeerConsole extends Application
 	/**
 	 * Run the CLI directly
 	 *
+	 * @param array $runtime Runtime configuration
+	 *
 	 * @return integer
 	 */
-	public static function makeAndRun()
+	public static function create($runtime = array())
 	{
-		return static::make()->run();
+		return static::make($runtime)->run();
 	}
 }
