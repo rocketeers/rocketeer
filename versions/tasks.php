d842110d39325ac01ac469a96b4f3a5232ec10c3
Maxime Fabre <ehtnam6@gmail.com>
2013-12-03 17:48:05 +0100

    Fix bower

--- versions/tasks.php
+++ versions/tasks.php
@@ -3,5 +3,5 @@ use Rocketeer\Facades\Rocketeer;
 
 Rocketeer::after('deploy', function ($task) {
 	$task->command->comment('Building assets');
-	$task->runForCurrentRelease(['bower install', 'npm install', 'node node_modules/.bin/grunt production']);
+	$task->runForCurrentRelease(['bower install --allow-root', 'npm install', 'node node_modules/.bin/grunt production']);
 });
