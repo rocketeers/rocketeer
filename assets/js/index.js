8b75f1c484b97ce432eb4afaf488e7e8290f995d
Maxime Fabre <ehtnam6@gmail.com>
2013-12-03 18:11:12 +0100

    Make link to Phars be download links

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,6 +17,9 @@ var appendFile = function(file) {
 
 	$('.layout-container').append(file);
 	Rainbow.color();
+
+	// Replace download links
+	$('a[href="http://rocketeer.autopergamene.eu/versions/rocketeer"]').attr('target', '_blank').attr('download', '');
 };
 
 // Get each Markdown file, parse it and add it to HTML
