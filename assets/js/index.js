205a402266dfa067241f6811cce497b8a4a3e8a3
Maxime Fabre <ehtnam6@gmail.com>
2013-12-03 02:44:01 +0100

    Fix some bugs

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,25 +12,19 @@ var parse = function(file) {
 	return file;
 };
 
+var appendFile = function(file) {
+	file = '<section>' +parse(file)+ '</section>';
+
+	$('.layout-container').append(file);
+	Rainbow.color();
+};
+
 // Get each Markdown file, parse it and add it to HTML
 ['rocketeer/README.md', 'wiki/Getting-started.md', 'wiki/Tasks.md'].forEach(function(file) {
 	$.ajax({
-		type: 'GET',
-		async: false,
-		url: file,
-		success: function(file) {
-
-			// Add file to page
-			$('.layout-container').append('<section>' + parse(file) + '</section>');
-		}
-	});
-});
-
-// Highlight codeblocks
-$('code').each(function(key, pre) {
-	var content = $(pre).html();
-
-	return Rainbow.color(content, 'php', function(highlighted) {
-		return $(pre).html(highlighted);
+		type    : 'GET',
+		async   : false,
+		url     : file,
+		success : appendFile
 	});
 });
