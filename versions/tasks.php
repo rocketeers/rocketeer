1e14db0c8663b61393915a719eac8fa4221262e3
Maxime Fabre <ehtnam6@gmail.com>
2013-12-03 18:11:57 +0100

    Add comments to deployment

--- versions/tasks.php
+++ versions/tasks.php
@@ -2,6 +2,9 @@
 use Rocketeer\Facades\Rocketeer;
 
 Rocketeer::after('deploy', function ($task) {
+	$task->command->comment('Installing components');
+	$task->runForCurrentRelease(['bower install --allow-root', 'npm install']);
+
 	$task->command->comment('Building assets');
-	$task->runForCurrentRelease(['bower install --allow-root', 'npm install', 'node node_modules/.bin/grunt production']);
+	$task->runForCurrentRelease('node node_modules/.bin/grunt production');
 });
