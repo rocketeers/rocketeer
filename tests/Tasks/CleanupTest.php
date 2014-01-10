b8865ebad17b5485a32d86c81b1f841ab37cff5e
Maxime Fabre <ehtnam6@gmail.com>
2014-01-10 19:17:33 +0100

    Add test for clean all

--- tests/Tasks/CleanupTest.php
+++ tests/Tasks/CleanupTest.php
@@ -18,6 +18,24 @@ class CleanupTest extends RocketeerTestCase
 		$this->assertTaskOutput('Cleanup', 'Removing <info>2 releases</info> from the server');
 	}
 
+	public function testCanPruneAllReleasesIfCleanAll()
+	{
+		$this->mockReleases(function ($mock) {
+			return $mock
+				->shouldReceive('getDeprecatedReleases')->never()
+				->shouldReceive('getNonCurrentReleases')->once()->andReturn(array(1, 2))
+				->shouldReceive('getPathToRelease')->times(2)->andReturnUsing(function ($release) {
+					return $release;
+				});
+		});
+
+		$this->assertTaskOutput('Cleanup', 'Removing <info>2 releases</info> from the server', $this->getCommand(array(), array(
+			'clean-all' => true,
+			'verbose'   => true,
+			'pretend'   => false,
+		)));
+	}
+
 	public function testPrintsMessageIfNoCleanup()
 	{
 		$this->mockReleases(function ($mock) {
