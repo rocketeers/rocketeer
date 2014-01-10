4f5250bc0f477ffd0450791e86e6bceeabe9b602
Maxime Fabre <ehtnam6@gmail.com>
2014-01-10 17:52:25 +0100

    Fix path to PHP in tests

--- tests/Tasks/UpdateTest.php
+++ tests/Tasks/UpdateTest.php
@@ -14,27 +14,27 @@ class UpdateTest extends RocketeerTestCase
 
 		$php     = exec('which php');
 		$matcher = array(
-		  array(
-		    "cd " .$this->server. "/releases/20000000000000",
-		    "git reset --hard",
-		    "git pull"
-		  ),
-		  "mkdir -p " .$this->server. "/shared/tests",
-		  "mv " .$this->server. "/releases/20000000000000/tests/Elements " .$this->server. "/shared/tests/Elements",
-		  array(
-		    "cd " .$this->server. "/releases/20000000000000",
-		    "chmod -R 755 " .$this->server. "/releases/20000000000000/tests",
-		    "chmod -R g+s " .$this->server. "/releases/20000000000000/tests",
-		    "chown -R www-data:www-data " .$this->server. "/releases/20000000000000/tests"
-		  ),
-		  array(
-		    "cd " .$this->server. "/releases/20000000000000",
-		    "/usr/local/bin/php artisan migrate --seed"
-		  ),
-		  array(
-		    "cd " .$this->server. "/releases/20000000000000",
-		    "/usr/local/bin/php artisan cache:clear"
-		  )
+			array(
+				"cd " .$this->server. "/releases/20000000000000",
+				"git reset --hard",
+				"git pull"
+			),
+			"mkdir -p " .$this->server. "/shared/tests",
+			"mv " .$this->server. "/releases/20000000000000/tests/Elements " .$this->server. "/shared/tests/Elements",
+			array(
+				"cd " .$this->server. "/releases/20000000000000",
+				"chmod -R 755 " .$this->server. "/releases/20000000000000/tests",
+				"chmod -R g+s " .$this->server. "/releases/20000000000000/tests",
+				"chown -R www-data:www-data " .$this->server. "/releases/20000000000000/tests"
+			),
+			array(
+				"cd " .$this->server. "/releases/20000000000000",
+				$php. " artisan migrate --seed"
+			),
+			array(
+				"cd " .$this->server. "/releases/20000000000000",
+				$php. " artisan cache:clear"
+			)
 		);
 
 		$this->assertTaskHistory($task, $matcher);
