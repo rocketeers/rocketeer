e47eab59180a15b764a80210520ef64f4ea5bd00
Maxime Fabre <ehtnam6@gmail.com>
2014-01-13 10:55:02 +0100

    Fix download attribute for phar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
 Rainbow.color();
 
 // Replace download links
-$('a[href="http://rocketeer.autopergamene.eu/versions/rocketeer"]').attr('target', '_blank').attr('download', '');
+$('a[href="http://rocketeer.autopergamene.eu/versions/rocketeer.phar"]').attr('target', '_blank').attr('download', '');
