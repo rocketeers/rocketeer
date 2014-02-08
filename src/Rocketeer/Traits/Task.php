b17fa10bea4a640da74450fce315547e12b495b0
Maxime Fabre <ehtnam6@gmail.com>
2014-02-08 17:41:12 +0100

    Fix before event not canceling task

--- src/Rocketeer/Traits/Task.php
+++ src/Rocketeer/Traits/Task.php
@@ -92,9 +92,11 @@ abstract class Task extends Bash
 	 */
 	public function fire()
 	{
-		$this->fireEvent('before');
-		$results = $this->execute();
-		$this->fireEvent('after');
+		// Fire the queue if the before event passes
+		if ($this->fireEvent('before')) {
+			$results = $this->execute();
+			$this->fireEvent('after');
+		}
 
 		return $results;
 	}
