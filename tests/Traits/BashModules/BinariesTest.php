8ae1e1d9737c4c3c6fcfedf3efdd2c12b4e117b3
Maxime Fabre <ehtnam6@gmail.com>
2014-08-21 13:44:56 +0200

    Remove deprecated test

--- tests/Traits/BashModules/BinariesTest.php
+++ tests/Traits/BashModules/BinariesTest.php
@@ -39,15 +39,6 @@ class BinariesTest extends RocketeerTestCase
 		$this->assertEquals($whichGrep, $grep);
 	}
 
-	public function testCanGetFallbackForBinary()
-	{
-		$whichGrep = exec('which grep');
-		$grep      = $this->task->which('foobar', $whichGrep);
-
-		$this->assertEquals($whichGrep, $grep);
-		$this->assertFalse($this->task->which('fdsf'));
-	}
-
 	public function testCanRunComposer()
 	{
 		$this->usesComposer(true);
