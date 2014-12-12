674da715239b197ffe45801f3a0643b6374c5136
Maxime Fabre <ehtnam6@gmail.com>
2014-12-12 17:17:43 +0100

    Missing licenses

--- src/Rocketeer/Tasks/Plugins/Installer.php
+++ src/Rocketeer/Tasks/Plugins/Installer.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of Rocketeer
+ *
+ * (c) Maxime Fabre <ehtnam6@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Rocketeer\Tasks\Plugins;
 
 use Rocketeer\Abstracts\AbstractTask;
