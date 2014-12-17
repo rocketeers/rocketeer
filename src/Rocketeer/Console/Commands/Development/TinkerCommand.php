f1e5ffc586c46f81ab9b236dc2b6bd61aca327e8
Maxime Fabre <ehtnam6@gmail.com>
2014-12-17 01:15:29 +0100

    Move Tinker command

--- src/Rocketeer/Console/Commands/Development/TinkerCommand.php
+++ src/Rocketeer/Console/Commands/Development/TinkerCommand.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Rocketeer\Console\Commands;
+namespace Rocketeer\Console\Commands\Development;
 
 use Boris\Boris;
 use Rocketeer\Abstracts\AbstractCommand;
