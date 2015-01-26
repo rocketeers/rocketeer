a95101fee729a91ec2c016aeb723355007a64bc0
Maxime Fabre <ehtnam6@gmail.com>
2015-01-26 01:51:45 +0100

    Fix missing pretend mode

--- tests/Strategies/Dependencies/PolyglotStrategyTest.php
+++ tests/Strategies/Dependencies/PolyglotStrategyTest.php
@@ -9,6 +9,7 @@ class PolyglotStrategyTest extends RocketeerTestCase
 {
     public function testCanInstallAllDependencies()
     {
+        $this->pretend();
         $this->usesComposer(true);
         $this->files->put($this->server.'/current/Gemfile', '');
 
