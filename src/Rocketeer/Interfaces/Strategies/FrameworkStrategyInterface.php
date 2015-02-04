c569b7c6315e54a923f37d2df1bbf4dff9518345
Maxime Fabre <ehtnam6@gmail.com>
2015-02-04 22:12:03 +0100

    Add some licenses

--- src/Rocketeer/Interfaces/Strategies/FrameworkStrategyInterface.php
+++ src/Rocketeer/Interfaces/Strategies/FrameworkStrategyInterface.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This file is part of Rocketeer
+ *
+ * (c) Maxime Fabre <ehtnam6@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Rocketeer\Interfaces\Strategies;
 
 use Symfony\Component\Console\Command\Command;
