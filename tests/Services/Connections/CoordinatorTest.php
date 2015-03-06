d561ac20e67db706f0c2b49c2c9527228e2378b6
Maxime Fabre <ehtnam6@gmail.com>
2015-03-06 17:31:22 +0100

    Remove unused dependency

--- tests/Services/Connections/CoordinatorTest.php
+++ tests/Services/Connections/CoordinatorTest.php
@@ -7,31 +7,31 @@ class CoordinatorTest extends RocketeerTestCase
 {
     public function testCanCoordinateTasks()
     {
-        $this->expectOutputString(
-            'A:production/a.com/develop'.PHP_EOL.
-            'A:production/a.com/master'.PHP_EOL.
-            'A:production/b.com/develop'.PHP_EOL.
-            'A:production/b.com/master'.PHP_EOL.
-            'A:staging/a.com/develop'.PHP_EOL.
-            'A:staging/a.com/master'.PHP_EOL.
-            'A:staging/b.com/develop'.PHP_EOL.
-            'A:staging/b.com/master'.PHP_EOL.
-            'B:staging/a.com/master'.PHP_EOL.
-            'B:staging/b.com/develop'.PHP_EOL.
-            'B:staging/b.com/master'.PHP_EOL.
-            'B:staging/a.com/develop'.PHP_EOL.
-            'B:production/b.com/master'.PHP_EOL.
-            'B:production/a.com/master'.PHP_EOL.
-            'B:production/b.com/develop'.PHP_EOL.
-            'B:production/a.com/develop'.PHP_EOL.
-            'C:production/a.com/master'.PHP_EOL.
-            'C:production/b.com/develop'.PHP_EOL.
-            'C:production/a.com/develop'.PHP_EOL.
-            'C:production/b.com/master'.PHP_EOL.
-            'C:staging/a.com/develop'.PHP_EOL.
-            'C:staging/b.com/develop'.PHP_EOL.
-            'C:staging/b.com/master'.PHP_EOL.
-            'C:staging/a.com/master'.PHP_EOL
+        $this->expectOutputRegex('#'.
+            'A:production/a.com/(master|develop)'.PHP_EOL.
+            'A:production/a.com/(master|develop)'.PHP_EOL.
+            'A:production/b.com/(master|develop)'.PHP_EOL.
+            'A:production/b.com/(master|develop)'.PHP_EOL.
+            'A:staging/a.com/(master|develop)'.PHP_EOL.
+            'A:staging/a.com/(master|develop)'.PHP_EOL.
+            'A:staging/b.com/(master|develop)'.PHP_EOL.
+            'A:staging/b.com/(master|develop)'.PHP_EOL.
+            'B:staging/a.com/(master|develop)'.PHP_EOL.
+            'B:staging/b.com/(master|develop)'.PHP_EOL.
+            'B:staging/b.com/(master|develop)'.PHP_EOL.
+            'B:staging/a.com/(master|develop)'.PHP_EOL.
+            'B:production/b.com/(master|develop)'.PHP_EOL.
+            'B:production/a.com/(master|develop)'.PHP_EOL.
+            'B:production/b.com/(master|develop)'.PHP_EOL.
+            'B:production/a.com/(master|develop)'.PHP_EOL.
+            'C:production/a.com/(master|develop)'.PHP_EOL.
+            'C:production/b.com/(master|develop)'.PHP_EOL.
+            'C:production/a.com/(master|develop)'.PHP_EOL.
+            'C:production/b.com/(master|develop)'.PHP_EOL.
+            'C:staging/a.com/(master|develop)'.PHP_EOL.
+            'C:staging/b.com/(master|develop)'.PHP_EOL.
+            'C:staging/b.com/(master|develop)'.PHP_EOL.
+            'C:staging/a.com/(master|develop)'.PHP_EOL.'#'
         );
 
         $this->swapConfig(array(
