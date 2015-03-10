c2ec1290d51a2a482d3d58e18faa1a9aaaf075d7
Maxime Fabre <ehtnam6@gmail.com>
2015-03-10 22:00:29 +0100

    Fix plugins folder being parsed for config

--- src/Rocketeer/Services/Config/ConfigurationLoader.php
+++ src/Rocketeer/Services/Config/ConfigurationLoader.php
@@ -166,7 +166,7 @@ class ConfigurationLoader
     {
         /** @type SplFileInfo[] $files */
         $files = $this->getFinder($folder)
-                      ->exclude(['connections', 'stages', 'tasks', 'events', 'strategies']);
+                      ->exclude(['connections', 'stages', 'plugins', 'tasks', 'events', 'strategies']);
 
         // Load base files
         foreach ($files as $file) {
