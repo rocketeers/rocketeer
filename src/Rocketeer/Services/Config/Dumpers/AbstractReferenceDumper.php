d064ba1945d41c46e54390e86e7502a3f5e7abca
Maxime Fabre <ehtnam6@gmail.com>
2015-03-11 21:24:00 +0100

    Split some stuff in AbstractReferenceDumper

--- src/Rocketeer/Services/Config/Dumpers/AbstractReferenceDumper.php
+++ src/Rocketeer/Services/Config/Dumpers/AbstractReferenceDumper.php
@@ -22,13 +22,38 @@ use Symfony\Component\Config\Definition\PrototypedArrayNode;
 abstract class AbstractReferenceDumper
 {
     /**
+     * @type array
+     */
+    protected $comments = [];
+
+    /**
+     * @type string
+     */
+    protected $default;
+
+    /**
+     * @type array
+     */
+    protected $defaultArray;
+
+    /**
+     * @type string|string[]
+     */
+    protected $example;
+
+    /**
+     * @type boolean
+     */
+    protected $isCoreNode;
+
+    /**
      * @type string
      */
     protected $reference;
 
     /**
      * @param ConfigurationInterface $configuration
-     * @param string null            $namespace
+     * @param                        string null            $namespace
      *
      * @return string
      */
@@ -60,12 +85,13 @@ abstract class AbstractReferenceDumper
      */
     private function writeNode(NodeInterface $node, $depth = 0)
     {
-        $comments     = [];
-        $default      = '';
-        $defaultArray = null;
-        $children     = null;
-        $example      = $node->getExample();
-        $isCoreNode   = $node->getParent() && $node->getParent()->getName() === 'rocketeer';
+        // Reinitialize fields
+        $this->comments     = [];
+        $this->default      = '';
+        $this->defaultArray = null;
+        $children           = null;
+        $this->example      = $node->getExample();
+        $this->isCoreNode   = $node->getParent() && $node->getParent()->getName() === 'rocketeer';
 
         if ($node instanceof ArrayNode) {
             $children = $node->getChildren();
@@ -91,31 +117,31 @@ abstract class AbstractReferenceDumper
                 }
 
                 if (!$children) {
-                    if ($node->hasDefaultValue() && count($defaultArray = $node->getDefaultValue())) {
-                        $default = '';
-                    } elseif (!is_array($example)) {
-                        $default = [];
+                    if ($node->hasDefaultValue() && count($this->defaultArray = $node->getDefaultValue())) {
+                        $this->default = '';
+                    } elseif (!is_array($this->example)) {
+                        $this->default = [];
                     }
                 }
             }
         } elseif ($node instanceof EnumNode) {
-            $comments[] = 'One of '.implode(', ', array_map('json_encode', $node->getValues()));
-            $default    = $node->getDefaultValue();
+            $this->comments[] = 'One of '.implode(', ', array_map('json_encode', $node->getValues()));
+            $this->default    = $node->getDefaultValue();
         } elseif ($node instanceof ClosureNode) {
-            $default  = $node->getDefaultValue();
-            $analyzer = new TokenAnalyzer();
-            $default  = $analyzer->analyze($default)['code'];
+            $this->default = $node->getDefaultValue();
+            $analyzer      = new TokenAnalyzer();
+            $this->default = $analyzer->analyze($this->default)['code'];
         } else {
-            $default = null;
+            $this->default = null;
 
             if ($node->hasDefaultValue()) {
-                $default = $node->getDefaultValue();
+                $this->default = $node->getDefaultValue();
 
-                if (is_array($default)) {
-                    if (count($defaultArray = $node->getDefaultValue())) {
-                        $default = '';
-                    } elseif (!is_array($example)) {
-                        $default = [];
+                if (is_array($this->default)) {
+                    if (count($this->defaultArray = $node->getDefaultValue())) {
+                        $this->default = '';
+                    } elseif (!is_array($this->example)) {
+                        $this->default = [];
                     }
                 }
             }
@@ -123,76 +149,44 @@ abstract class AbstractReferenceDumper
 
         // required?
         if ($node->isRequired()) {
-            $comments[] = 'Required';
+            $this->comments[] = 'Required';
         }
 
         // example
-        if ($example && !is_array($example)) {
-            $comments[] = 'Example: '.$example;
+        if ($this->example && !is_array($this->example)) {
+            $this->comments[] = 'Example: '.$this->example;
         }
 
         // Format comments and values
-        $comments = count($comments) ? '// '.implode(', ', $comments) : '';
-        $name     = $this->serializeValue($node->getName()).' => ';
-        $format   = '%-20s %s %s';
+        $this->comments = count($this->comments) ? '// '.implode(', ', $this->comments) : '';
+        $name           = $this->serializeValue($node->getName()).' => ';
+        $format         = '%-20s %s %s';
 
         if ($node instanceof ArrayNode) {
             $name .= '[';
-            $default = (!$example && !$children && !$defaultArray) ? '],' : null;
-            $format  = '%s%s %s';
+            $this->default = (!$this->example && !$children && !$this->defaultArray) ? '],' : null;
+            $format        = '%s%s %s';
         } elseif ($node instanceof ClosureNode) {
-            $default .= ',';
+            $this->default .= ',';
         } else {
-            $default = $default === "\n" ? '"\n"' : $this->serializeValue($default);
-            $default .= ',';
+            $this->default = $this->default === "\n" ? '"\n"' : $this->serializeValue($this->default);
+            $this->default .= ',';
         }
 
         // Output informations
-        if ($info = $node->getInfo()) {
-            $this->writeLine('');
+        $this->outputInformations($node, $depth);
 
-            $info = str_replace("\n", sprintf("\n%".($depth * 4)."s// ", ' '), $info);
-            $this->writeComment($info, $depth * 4);
-
-            if ($isCoreNode) {
-                $this->writeLine(str_repeat('/', 70), $depth * 4);
-                $this->writeLine('');
-            }
-        }
-
-        $default = str_replace("\n", sprintf("\n%".($depth * 4)."s ", ' '), $default);
-        $text    = rtrim(sprintf($format, $name, $default, $comments), ' ');
+        $this->default = str_replace("\n", sprintf("\n%".($depth * 4)."s ", ' '), $this->default);
+        $text          = rtrim(sprintf($format, $name, $this->default, $this->comments), ' ');
 
         // Output main value
         $this->writeLine($text, $depth * 4);
 
-        // Output defaults
-        if ($defaultArray) {
-            $message    = count($defaultArray) > 1 ? 'Defaults' : 'Default';
-            $childDepth = $depth * 4 + 4;
-
-            $this->writeComment($message.':', $childDepth);
-            $this->writeArray($defaultArray, $childDepth);
-        }
-
-        // Output examples
-        if (is_array($example)) {
-            $message = count($example) > 1 ? 'Examples' : 'Example';
-
-            $this->writeComment($message.':', $depth * 4 + 4);
-            $this->writeArray($example, $depth * 4 + 4, true);
-        }
+        $this->outputDefaults($depth);
+        $this->outputExamples($depth);
 
         if ($node instanceof ArrayNode) {
-            if ($children) {
-                foreach ($children as $childNode) {
-                    $this->writeNode($childNode, $depth + 1);
-                }
-            }
-
-            if ($children || $example || $defaultArray) {
-                $this->writeLine('],', $depth * 4);
-            }
+            $this->outputChildren($children, $depth);
         }
     }
 
@@ -229,6 +223,83 @@ abstract class AbstractReferenceDumper
             }
         }
     }
+
+    //////////////////////////////////////////////////////////////////////
+    ////////////////////////////// OUTPUT ////////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+
+    /**
+     * Output the node's examples
+     *
+     * @param $depth
+     */
+    private function outputExamples($depth)
+    {
+        if (is_array($this->example)) {
+            $message = count($this->example) > 1 ? 'Examples' : 'Example';
+
+            $this->writeComment($message.':', $depth * 4 + 4);
+            $this->writeArray($this->example, $depth * 4 + 4, true);
+        }
+    }
+
+    /**
+     * Output the node's default value
+     *
+     * @param integer $depth
+     */
+    private function outputDefaults($depth)
+    {
+        if ($this->defaultArray) {
+            $message    = count($this->defaultArray) > 1 ? 'Defaults' : 'Default';
+            $childDepth = $depth * 4 + 4;
+
+            $this->writeComment($message.':', $childDepth);
+            $this->writeArray($this->defaultArray, $childDepth);
+        }
+    }
+
+    /**
+     * Output the children of the node
+     *
+     * @param  array  $children
+     * @param integer $depth
+     */
+    private function outputChildren($children, $depth)
+    {
+        if ($children) {
+            foreach ($children as $childNode) {
+                $this->writeNode($childNode, $depth + 1);
+            }
+        }
+
+        if ($children || $this->example || $this->defaultArray) {
+            $this->writeLine('],', $depth * 4);
+        }
+    }
+
+    /**
+     * @param NodeInterface $node
+     * @param integer       $depth
+     */
+    private function outputInformations(NodeInterface $node, $depth)
+    {
+        if ($info = $node->getInfo()) {
+            $this->writeLine('');
+
+            $info = str_replace("\n", sprintf("\n%".($depth * 4)."s// ", ' '), $info);
+            $this->writeComment($info, $depth * 4);
+
+            if ($this->isCoreNode) {
+                $this->writeLine(str_repeat('/', 70), $depth * 4);
+                $this->writeLine('');
+            }
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////
+    ////////////////////////////// HELPERS ///////////////////////////////
+    //////////////////////////////////////////////////////////////////////
 
     /**
      * @param mixed $value
