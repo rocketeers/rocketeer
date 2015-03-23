6536346f03b70913d22a2932660c758c193e14f1
Maxime Fabre <ehtnam6@gmail.com>
2015-03-23 23:42:57 +0100

    Fix version constraint and constants

--- src/Rocketeer/Services/Connections/Gateways/SeclibGateway.php
+++ src/Rocketeer/Services/Connections/Gateways/SeclibGateway.php
@@ -154,7 +154,7 @@ class SeclibGateway implements GatewayInterface
      */
     public function put($local, $remote)
     {
-        $this->getConnection()->put($remote, $local, Net_SFTP::SOURCE_LOCAL_FILE);
+        $this->getConnection()->put($remote, $local, NET_SFTP_LOCAL_FILE);
     }
 
     /**
@@ -175,7 +175,7 @@ class SeclibGateway implements GatewayInterface
      */
     public function nextLine()
     {
-        $value = $this->getConnection()->_get_channel_packet(Net_SSH2::CHANNEL_EXEC);
+        $value = $this->getConnection()->_get_channel_packet(NET_SSH2_CHANNEL_EXEC);
 
         return $value === true ? null : $value;
     }
