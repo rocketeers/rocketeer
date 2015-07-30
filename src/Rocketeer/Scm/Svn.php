ec3378d0c05116f30c4a9357c7ae17f11d570f00
vitr <admin@vitr.biz>
2015-07-30 11:56:49 +1000

    Username for subversion can't have a dot in it #559

--- src/Rocketeer/Scm/Svn.php
+++ src/Rocketeer/Scm/Svn.php
@@ -79,7 +79,7 @@ class Svn extends AbstractBinary implements ScmInterface
         $branch     = $this->connections->getRepositoryBranch();
         $repository = $this->connections->getRepositoryEndpoint();
         $repository = rtrim($repository, '/').'/'.ltrim($branch, '/');
-        $repository = preg_replace('#//[a-zA-Z0-9]+:?[a-zA-Z0-9]*@#', '//', $repository);
+        $repository = preg_replace('#//[a-zA-Z0-9.]+:?[a-zA-Z0-9]*@#', '//', $repository);
 
         return $this->co([$repository, $destination], $this->getCredentials());
     }
