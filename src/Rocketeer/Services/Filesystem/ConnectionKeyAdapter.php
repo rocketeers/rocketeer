0ab9721b4124feba1401135bdf95c6acbb6cfc1e
Maxime Fabre <ehtnam6@gmail.com>
2016-07-12 22:16:57 +0200

    This is all in SFTP now

--- src/Rocketeer/Services/Filesystem/ConnectionKeyAdapter.php
+++ src/Rocketeer/Services/Filesystem/ConnectionKeyAdapter.php
@@ -13,23 +13,15 @@
 namespace Rocketeer\Services\Filesystem;
 
 use League\Flysystem\Sftp\SftpAdapter;
-use phpseclib\System\SSH\Agent;
 use Rocketeer\Services\Connections\Credentials\Keys\ConnectionKey;
 
 class ConnectionKeyAdapter extends SftpAdapter
 {
     /**
-     * @var bool
-     */
-    protected $agent;
-
-    /**
      * @param ConnectionKey $connectionKey
      */
     public function __construct(ConnectionKey $connectionKey)
     {
-        $this->configurable[] = 'agent';
-
         parent::__construct([
             'host' => $connectionKey->host,
             'username' => $connectionKey->username,
@@ -39,54 +31,5 @@ class ConnectionKeyAdapter extends SftpAdapter
             'root' => '/',
             'timeout' => 60 * 60,
         ]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function login()
-    {
-        parent::login();
-
-        $authentication = $this->getPassword();
-        if ($authentication instanceof Agent) {
-            $authentication->startSSHForwarding($this->connection);
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getPassword()
-    {
-        if ($this->agent) {
-            return $this->getAgent();
-        }
-
-        return parent::getPassword();
-    }
-
-    /**
-     * @return Agent|bool
-     */
-    public function getAgent()
-    {
-        if ($this->agent === true) {
-            $this->agent = new Agent();
-        }
-
-        return $this->agent;
-    }
-
-    /**
-     * @param bool $agent
-     *
-     * @return $this
-     */
-    public function setAgent($agent)
-    {
-        $this->agent = (bool) $agent;
-
-        return $this;
     }
 }
