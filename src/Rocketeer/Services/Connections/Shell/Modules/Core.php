f87d6eac64d8d1ed3b2f15a182112d2820964d52
Maxime Fabre <ehtnam6@gmail.com>
2016-07-21 16:57:37 +0200

    Split statuses into different module

--- src/Rocketeer/Services/Connections/Shell/Modules/Core.php
+++ src/Rocketeer/Services/Connections/Shell/Modules/Core.php
@@ -200,47 +200,6 @@ class Core extends AbstractBashModule
         return $this->run($tasks);
     }
 
-    /**
-     * Check the status of the last command.
-     *
-     * @return bool
-     */
-    public function status()
-    {
-        return $this->getOption('pretend') ? true : $this->getConnection()->status() === 0;
-    }
-
-    /**
-     * Check the status of the last run command, return an error if any.
-     *
-     * @param string      $error   The message to display on error
-     * @param string|null $output  The command's output
-     * @param string|null $success The message to display on success
-     *
-     * @return bool
-     */
-    public function checkStatus($error, $output = null, $success = null)
-    {
-        // If all went well
-        if ($this->status()) {
-            if ($success) {
-                $this->explainer->success($success);
-            }
-
-            return $output || true;
-        }
-
-        // Else display the error
-        $error = sprintf('An error occured: "%s"', $error);
-        if ($output) {
-            $error .= ', while running:'.PHP_EOL.$output;
-        }
-
-        $this->explainer->error($error);
-
-        return false;
-    }
-
     ////////////////////////////////////////////////////////////////////
     /////////////////////////////// HELPERS ////////////////////////////
     ////////////////////////////////////////////////////////////////////
---
+++ src/Rocketeer/Services/Connections/Shell/Modules/Statuses.php
@@ -0,0 +1,58 @@
+<?php
+namespace Rocketeer\Services\Connections\Shell\Modules;
+
+class Statuses extends AbstractBashModule
+{
+
+    /**
+     * Check the status of the last command.
+     *
+     * @return bool
+     */
+    public function status()
+    {
+        return $this->getOption('pretend') ? true : $this->getConnection()->status() === 0;
+    }
+
+    /**
+     * Check the status of the last run command, return an error if any.
+     *
+     * @param string      $error   The message to display on error
+     * @param string|null $output  The command's output
+     * @param string|null $success The message to display on success
+     *
+     * @return bool
+     */
+    public function checkStatus($error, $output = null, $success = null)
+    {
+        // If all went well
+        if ($this->status()) {
+            if ($success) {
+                $this->explainer->success($success);
+            }
+
+            return $output || true;
+        }
+
+        // Else display the error
+        $error = sprintf('An error occured: "%s"', $error);
+        if ($output) {
+            $error .= ', while running:'.PHP_EOL.$output;
+        }
+
+        $this->explainer->error($error);
+
+        return false;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getProvided()
+    {
+        return [
+            'status',
+            'checkStatus',
+        ];
+    }
+}
